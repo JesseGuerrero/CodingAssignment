0efde51a36491ebbd22422d14ea026bbcdfd1995
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-08-03 16:59:46 -0500

    Created a Table component and started on a first draft within App.js

---
+++ src/components/Table.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <div style={{width: "100%"}}>
+            <table style={{margin: "0 auto"}} border="1">
+                <tr>
+                    <th>Contact ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>E-mail</th>
+                    <th>Phone Number</th>
+                    <th>Country Code</th>
+                </tr>
+                <tr>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                    <td>Data 3</td>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                    <td>Data 3</td>
+                </tr>
+                <tr>
+                    <td>Data 4</td>
+                    <td>Data 5</td>
+                    <td>Data 6</td>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                    <td>Data 3</td>
+                </tr>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
